1bf50268bccee213ceda8a4fe8c2c21d9640bf4f
LWSS <bbchallenger100@gmail.com>
2026-09-24 20:01:00 -0700

    hud_sounds

--- source/interface/hud_definitions.h
+++ source/interface/hud_definitions.h
@@ -8,11 +8,28 @@ header included in hcex build.
 #define __HUD_DEFINITIONS_H
 #pragma once
 
+/* ---------- headers */
+
+#include "real_math.h"
+#include "tag_files.h"
+#include "tag_groups.h"
+
 /* ---------- constants */
 
 /* ---------- macros */
 
 /* ---------- structures */
+
+struct sound_hud_element_definition
+{
+	struct tag_reference sound;
+	long type_flags;
+	real scale;
+	long unused[3];
+	byte pad0;
+	byte pad[3];
+	long unused2[4];
+};
 
 /* ---------- prototypes/EXAMPLE.C */
 
