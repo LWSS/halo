572be1af99827c93a4e56a5bc43e8dbd90c285c6
LWSS <LWSS@users.noreply.github.com>
2026-09-26 05:19:37 -0700

    scenery (#55)

--- source/models/models.h
+++ source/models/models.h
@@ -10,9 +10,27 @@ header included in hcex build.
 
 /* ---------- constants */
 
+enum animation_update_kind
+{
+	animation_update_kind_render_only = 0,
+	animation_update_kind_affects_game_state,
+};
+
+enum
+{
+	_animation_running = 0,
+	_animation_key_frame,
+	_animation_will_restart_on_next_frame,
+	_animation_restarted,
+	_animation_looped,
+	NUMBER_OF_ANIMATION_UPDATE_RESULTS,
+};
+
 /* ---------- macros */
 
 /* ---------- structures */
+
+struct animation_state;
 
 /* ---------- prototypes/MODELS.C */
 
@@ -30,8 +48,29 @@ short model_get_marker_by_name(
 	struct object_marker *markers,
 	short maximum_marker_count);
 
+/* ---------- prototypes/MODEL_ANIMATIONS.C */
+
+short animation_graph_get_animation_by_name(long animation_graph_index, char const *animation_name);
+short animation_choose_random_permutation_internal(enum animation_update_kind render_or_affects_game_state, long animation_graph_index, short animation_index);
+short animation_update_internal(enum animation_update_kind render_or_affects_game_state, long animation_graph_index, struct animation_state *state, long *triggered_sound_index);
+
 /* ---------- globals */
 
 /* ---------- public code */
+
+__inline short animation_choose_random_permutation(
+	long animation_graph_index,
+	short animation_index)
+{
+	return animation_choose_random_permutation_internal(animation_update_kind_affects_game_state, animation_graph_index, animation_index);
+}
+
+__inline short animation_update(
+	long animation_graph_index,
+	struct animation_state *state,
+	long *triggered_sound_index)
+{
+	return animation_update_internal(animation_update_kind_affects_game_state, animation_graph_index, state, triggered_sound_index);
+}
 
 #endif // __MODELS_H
--- source/objects/scenery.c
+++ source/objects/scenery.c
@@ -1,36 +1,17 @@
 /*
 SCENERY.C
 
-symbols in this file:
-00134D70 0010:
-	_scenery_initialize (0000)
-00134D80 0010:
-	_scenery_initialize_for_new_map (0000)
-00134D90 0010:
-	_scenery_dispose_from_old_map (0000)
-00134DA0 0010:
-	_scenery_dispose (0000)
-00134DB0 0020:
-	_scenery_place (0000)
-00134DD0 0080:
-	_scenery_new (0000)
-00134E50 0010:
-	_scenery_delete (0000)
-00134E60 0050:
-	_scenery_update (0000)
-00134EB0 00e0:
-	_code_00134eb0 (0000)
-00134F90 0070:
-	_scenery_get_animation_time (0000)
-00135000 0020:
-	_scenery_animation_start (0000)
-00135020 0020:
-	_scenery_animation_start_at_frame (0000)
-0028AC4C 0033:
-	??_C@_0DD@PMOLPF@the?5animation?5?8?$CFs?8?5doesn?8t?5exist@ (0000)
 */
 
 /* ---------- headers */
+
+#include "cseries.h"
+#include "scenery.h"
+
+#include "main/console.h"
+#include "models/models.h"
+#include "models/model_animation_definitions.h"
+#include "scenario/scenario_definitions.h"
 
 /* ---------- constants */
 
@@ -40,8 +21,162 @@ symbols in this file:
 
 /* ---------- prototypes */
 
+static void scenery_animation_start_private(
+	long scenery_index,
+	long animation_graph_index,
+	char const *animation_name,
+	short frame_index);
+
 /* ---------- globals */
 
 /* ---------- public code */
 
+void scenery_initialize(
+	void)
+{
+	return;
+}
+
+void scenery_initialize_for_new_map(
+	void)
+{
+	return;
+}
+
+void scenery_dispose_from_old_map(
+	void)
+{
+	return;
+}
+
+void scenery_dispose(
+	void)
+{
+	return;
+}
+
+void scenery_place(
+	long scenery_index,
+	struct scenario_scenery_datum *scenario_scenery)
+{
+	object_add_scenario_permutation(scenery_index, &scenario_scenery->permutation);
+
+	return;
+}
+
+boolean scenery_new(
+	long object_index)
+{
+	struct scenery_datum *scenery = scenery_get(object_index);
+	struct object_definition *definition = object_definition_get(scenery->definition_index);
+
+	if (definition->object.animation_graph.index != NONE &&
+		animation_graph_definition_get(definition->object.animation_graph.index)->animations.count > 0)
+	{
+		short animation_index = animation_choose_random_permutation(definition->object.animation_graph.index, 0);
+
+		if (animation_index != NONE)
+		{
+			scenery->object.animation.state.index = animation_index;
+			scenery->object.animation.animation_graph_index = definition->object.animation_graph.index;
+			SET_FLAG(scenery->object.flags, _object_animates_automatically_bit, TRUE);
+		}
+	}
+	SET_FLAG(scenery->object.flags, _object_shadowless_bit, TRUE);
+
+	return TRUE;
+}
+
+void scenery_delete(
+	long scenery_index)
+{
+	return;
+}
+
+boolean scenery_update(
+	long scenery_index)
+{
+	struct scenery_datum *scenery = scenery_get(scenery_index);
+
+	if (TEST_FLAG(scenery->scenery.flags, _scenery_self_animated_bit))
+	{
+		switch (animation_update(scenery->object.animation.animation_graph_index, &scenery->object.animation.state, NULL))
+		{
+		case _animation_will_restart_on_next_frame:
+			scenery->object.animation.state.frame_index--;
+			break;
+		}
+	}
+
+	return TRUE;
+}
+
+short scenery_get_animation_time(
+	long scenery_index)
+{
+	struct scenery_datum *scenery = scenery_get(scenery_index);
+
+	if (TEST_FLAG(scenery->scenery.flags, _scenery_self_animated_bit))
+	{
+		struct animation_graph *graph = animation_graph_definition_get(scenery->object.animation.animation_graph_index);
+		struct animation *animation = TAG_BLOCK_GET_ELEMENT(&graph->animations, scenery->object.animation.state.index, struct animation);
+
+		return MAX(animation->frame_count - scenery->object.animation.state.frame_index - 2, 0);
+	}
+
+	return 0;
+}
+
+void scenery_animation_start(
+	long scenery_index,
+	long animation_graph_index,
+	char const *animation_name)
+{
+	scenery_animation_start_private(scenery_index, animation_graph_index, animation_name, 0);
+
+	return;
+}
+
+void scenery_animation_start_at_frame(
+	long scenery_index,
+	long animation_graph_index,
+	char const *animation_name,
+	short frame_index)
+{
+	scenery_animation_start_private(scenery_index, animation_graph_index, animation_name, frame_index);
+
+	return;
+}
+
 /* ---------- private code */
+
+static void scenery_animation_start_private(
+	long scenery_index,
+	long animation_graph_index,
+	char const *animation_name,
+	short frame_index)
+{
+	if (scenery_index != NONE && animation_graph_index != NONE)
+	{
+		struct scenery_datum *scenery = scenery_get(scenery_index);
+		struct animation_graph *graph = animation_graph_definition_get(animation_graph_index);
+		short animation_index = animation_graph_get_animation_by_name(animation_graph_index, animation_name);
+
+		if (animation_index != NONE)
+		{
+			struct animation *animation = TAG_BLOCK_GET_ELEMENT(&graph->animations, animation_index, struct animation);
+
+			SET_FLAG(scenery->scenery.flags, _scenery_self_animated_bit, TRUE);
+			SET_FLAG(scenery->object.flags, _object_animates_automatically_bit, FALSE);
+			scenery->object.animation.state.index = animation_index;
+			scenery->object.animation.state.frame_index = PIN(frame_index, 0, animation->frame_count - 1);
+			scenery->object.animation.animation_graph_index = animation_graph_index;
+		}
+		else
+		{
+			console_warning("the animation '%s' doesn't exist in the graph '%s'", animation_name, tag_get_name(animation_graph_index));
+		}
+	}
+
+	return;
+}
